502a16e171e08dc7e6bce3584919b377fd0ea030
River Mochi <97197236+River-Mochi@users.noreply.github.com>
2026-08-28 16:28:05 -0700

    update shared lib .editorconfig to allow usings above namespace

--- Systems/TaxiTrafficSystem.Core.cs
+++ Systems/TaxiTrafficSystem.Core.cs
@@ -6,17 +6,13 @@
 // This notice MUST be kept with copies or substantial portions of this code.
 // ================= </copyright> ======================
 
+    using Game;              // GameSystemBase, GameMode
+
+
 namespace TaxiTraffic
 {
     // File: Systems/TaxiTrafficSystem.Core.cs
     // System lifecycle and update coordinator.
-    using Game;              // GameSystemBase, GameMode
-    using Game.Creatures;    // prevent gs errors partial files
-    using Game.Pathfind;     // prevent gs errors partial files
-    using Game.Vehicles;     // prevent gs errors partial files
-    using Unity.Collections; // prevent gs errors partial files
-    using Unity.Entities;    // prevent gs errors partial files
-
 
     public partial class TaxiTrafficSystem : GameSystemBase
     {
