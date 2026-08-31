7f65b1e87816fe5e01c6279449d6c4dbfa2f8aa1
River Mochi <97197236+River-Mochi@users.noreply.github.com>
2026-08-30 22:53:32 -0700

    update license block

--- Localization/LocaleZH_HANS.cs
+++ Localization/LocaleZH_HANS.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="LocaleZH_HANS.cs" company="River-Mochi">
+// <copyright file="LocaleZH_HANS.cs" company="River-Mochi">
 // Copyright (c) 2026 River-Mochi. All rights reserved.
 // Licensed under the GNU General Public License v3.0 or later,
 // with the Cities: Skylines II Linking Exception.
